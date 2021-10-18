b15d24af7292ad2b62cad28732a4fa14bef9be1d
JJaviergr1521 <jjaviergr@byui.edu>
2021-10-18 16:36:43 -0500

    ok

---
+++ rafting/css/get_noun.py
@@ -0,0 +1,16 @@
+def get_noun(grammatical_number):
+    """Return a randomly chosen noun.
+    If grammatical_number == 1, this function will
+    return one of these ten single nouns:
+        "adult", "bird", "boy", "car", "cat",
+        "child", "dog", "girl", "man", "woman"
+    Otherwise, this function will return one of these
+    ten plural nouns:
+        "adults", "birds", "boys", "cars", "cats",
+        "children", "dogs", "girls", "men", "women"
+
+    Parameter
+        grammatical_number: an integer that determines
+            if the returned noun is single or plural.
+    Return: a randomly chosen noun.
+    """
